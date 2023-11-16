c4c86cbde7db4e5e39cd050a2eda75a66f7548e5
Arnim Bleier <arnim.bleier@gmail.com>
2023-11-16 16:30:52 +0100

    update demo

--- demo.R
+++ demo.R
@@ -5,9 +5,10 @@
 library(ggplot2) 
 
 # create factors with value labels 
-mtcarsgear,levels=c(3,4,5), labels=c("3gears","4gears","5gears")) 
-mtcarsam,levels=c(0,1), labels=c("Automatic","Manual")) 
-mtcarscyl,levels=c(4,6,8), labels=c("4cyl","6cyl","8cyl")) 
+# create factors with value labels 
+mtcars$gear <- factor(mtcars$gear,levels=c(3,4,5), labels=c("3gears","4gears","5gears")) 
+mtcars$am <- factor(mtcars$am,levels=c(0,1), labels=c("Automatic","Manual")) 
+mtcars$cyl <- factor(mtcars$cyl,levels=c(4,6,8), labels=c("4cyl","6cyl","8cyl")) 
 
 # Kernel density plots for mpg
 # grouped by number of gears (indicated by color)
